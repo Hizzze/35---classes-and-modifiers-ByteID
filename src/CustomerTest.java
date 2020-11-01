bade1f9808f1c1773c714934d15d8e2b19996c0a
Non <mioleh666@gmail.com>
2020-11-01 23:25:33 +0000

    using getters and setters

--- src/CustomerTest.java
+++ src/CustomerTest.java
@@ -2,6 +2,9 @@ public class CustomerTest {
 
 	public static void main(String[] args) {
 		Сustomer c = new Сustomer();
+    c.setID(4);
+    c.setNew(false);
+    c.setTotal(313.24);
     c.displayCustomerInfo();
 	}
 
